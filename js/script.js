d555597a387839ccb2f73f819eb7dac9794986ee
Mulyadi Dharmawan <dharmawanmul@gmail.com>
2020-08-04 16:24:43 +0700

    Added "Pets" section with Isotope filter and Magnific plugins

--- js/script.js
+++ js/script.js
@@ -6,6 +6,32 @@ $(window).on('load', function() {
   $('#preloader').delay(350).fadeOut();
 });
 
+$(function() {
+  $('#myCarousel').carousel({
+    interval: 2000
+  });
+  var clickEvent = false;
+
+  $('#myCarousel').on('click', '.nav a', function () {
+    clickEvent = true;
+    $('.nav li').removeClass('nav-item active');
+    $(this).parent().addClass('nav-item active');
+  });
+
+  $('#myCarousel').bind('slid.bs.carousel', function () {
+  // Get currently selected item
+  var item = $('#myCarousel .carousel-inner .carousel-item.active');
+
+  // Deactivate all nav links
+  $('.nav li').removeClass('nav-item active');
+  
+  // Index is 1-based, use this to activate the nav link based on slide
+  var index = item.index() + 1;
+  
+  $('.nav li:nth-child(' + index + ')').addClass('nav-item active');
+  });
+});
+
 /* =================================================
                      Slider
 ===================================================*/
@@ -39,12 +65,51 @@ $(function() {
 });
 
 /* =================================================
-                  Responsive Tabs
+                Pets [Isotope Filter]
 ===================================================*/
-$(function() {
-  $('#services-tabs').responsiveTabs({
-    startCollapsed: 'accordion',
-    animation: 'slide',
-    duration: 300,
+$(window).on('load', function () {
+
+  // Initialize Isotope
+  $("#isotope-container").isotope({
+
+  });
+
+  // filter items on button click
+  $("#isotope-filters").on('click', 'button', function () {
+
+      // get filter value
+      var filterValue = $(this).attr('data-filter');
+
+      // filter portfolio
+      $("#isotope-container").isotope({
+          filter: filterValue
+      });
+
+      // active button
+      $("#isotope-filters").find('.active').removeClass('active');
+      $(this).addClass('active');
   });
 });
+/* =================================================
+                    Magnific
+===================================================*/
+$(function() {
+  $("#pets-wrapper").magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  });
+});
+
+/* =================================================
+                  Responsive Tabs
+===================================================*/
+// $(function() {
+//   $('#services-tabs').responsiveTabs({
+//     startCollapsed: 'accordion',
+//     animation: 'slide',
+//     duration: 300,
+//   });
+// });
